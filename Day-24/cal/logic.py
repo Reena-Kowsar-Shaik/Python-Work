8523bfb8454f55763eea9902812f10cf7320cec6
Reena-05 <reenakowsar05@gmail.com>
2026-09-05 15:31:27 +0530

    atm & cal

---
+++ Day-24/cal/logic.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    print('Adiition:',a+b)
+def sub(a,b):
+    print('Subtraction:',a-b)
+def mul(a,b):
+    print('Multiplication:',a*b)
+def div(a,b):
+    print('division:',a/b)
+def exp(a,b):
+    print('Exp:',a**b)
+def rem(a,b):
+    print('Rem:',a//b)
+    
---
+++ Day-24/cal/main.py
@@ -0,0 +1,28 @@
+'''import logic
+logic.add(3,4)
+logic.sub(8,4)
+logic.mul(4,5)
+logic.div(2,2)
+logic.rem(2,3)
+logic.exp(2,3)'''
+
+'''import logic as lg
+lg.add(3,4)
+lg.sub(8,4)
+lg.mul(4,5)
+lg.div(2,2)
+lg.rem(2,3)
+lg.exp(2,3)'''
+
+'''from logic import add,sub
+add(2,3)
+sub(4,2)'''
+
+from logic import *
+add(2,3)
+sub(4,5)
+mul(8,5)
+div(2,2)
+rem(2,3)
+exp(2,3)
+
