9edfba8079273755e1704e7abed18e1eecfbe4ab
Reena-05 <reenakowsar05@gmail.com>
2026-09-05 15:24:57 +0530

    nested-if

---
+++ Day-12/Nestedif.py
@@ -0,0 +1,9 @@
+n1=input()
+n2=input()
+if n1=='True':
+    if n2=='True':
+        print('Story Visible')
+    else:
+        print('Not in Close Friends List')
+else:
+        print('Not in Close Friends List')
